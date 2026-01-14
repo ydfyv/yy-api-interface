08e13f411824b056d67b81b46e56404e99b3a9aa
杨建成 <11967785+yang-jiancheng@user.noreply.gitee.com>
2026-01-14 18:50:26 +0800

    edit 动态生成路由接口

--- src/main/java/com/yy/yyapiinterface/api/DynamicRouteService.java
+++ src/main/java/com/yy/yyapiinterface/api/DynamicRouteService.java
@@ -3,17 +3,24 @@ package com.yy.yyapiinterface.api;
 public interface DynamicRouteService {
 
     /**
-     * 添加路由
+     * 新增路由
+     * @param id 接口方法名称
+     * @param uri 服务器uri
+     * @param patternPath 转发路径模式匹配
      */
-    void addRoute(String pathName);
+    void addRoute(String id, String uri, String patternPath);
 
     /**
-     * 删除路由
+     * 删除落雨
+     * @param id 接口方法名称
      */
     void deleteRoute(String id);
 
     /**
      * 更新路由
+     * @param id 接口方法名称
+     * @param uri 服务器uri
+     * @param patternPath 转发路径模式匹配
      */
-    void updateRoute(String id, String pathName);
+    void updateRoute(String id, String uri, String patternPath);
 }
